3553b0328b0e65dff72c3a1009295f903e4f9799
hfhfff <dyftfboys@163.com>
2024-09-14 10:52:19 +0800

    40 bit ,加入了all_done,trans_go,data_cnt,data1这些变量。 使用了“循环左移8位”的写法。现在我在思考， 变量“state”存在的意义是什么？

--- state.v
+++ state.v
@@ -3,16 +3,18 @@ input sys_clk,
 input rst_n,
 input Trans_go,//need to be assigned a value from top module
 input [39:0]Data,
-output uart_tx
+output uart_tx,
+output reg all_done
 );
 /*---------------------variate declaration---------------------------*/
 wire tx_done;
 reg state;
 reg [39:0]Data1;
-reg [2:0]Data_cnt; //40/8=5;3 bits binary digits can express to 8(D)
-reg all_done;
+reg [2:0]Data_cnt; //[2:0] =>3 bits binary digits can express to 8(D)
+
 reg send_go;
 reg [7:0]data;
+parameter max = 5;
 /*-----------------------instantiate serial's module-----------------------------*/ 
 send_byte serial_module(                           
         .sys_clk     (sys_clk  )    ,          
@@ -31,21 +33,21 @@ always @(posedge sys_clk or negedge rst_n) begin
     else if(Trans_go)begin
         state<=1;//send state
     end
-    else if(all_done==1)begin
+    else if(all_done)begin
         state <= 0;//back IDLE state
     end
+    else
+        state <= state;
 end
 /*----------------------Data_cnt--------------------------*/
 always @(posedge sys_clk or negedge rst_n) begin
     if (!rst_n) begin
         Data_cnt <= 0;
     end
-    else if (Data_cnt<3'd5) begin//40/8=5
-        if(tx_done)begin
-            Data_cnt<=Data_cnt+1;
-        end
+    else if (send_go) begin
+        Data_cnt<=Data_cnt+1;
     end
-    else if ((Data_cnt==3'd4)&&(tx_done))//clear to zero when add up to the biggest value 
+    else if ((Data_cnt==max)&&(tx_done))//clear to zero when add up to the biggest value 
         Data_cnt<=0;    
     else
         Data_cnt<=Data_cnt;
@@ -53,58 +55,49 @@ end
 /*----------------------Data1--------------------------*/
 always @(posedge sys_clk or negedge rst_n) begin
     if (!rst_n) begin
-        Data1 <= 40'd0;
+        Data1 <= Data;
     end
-    else if(state)//send state
-        if(tx_done)begin
-            Data1<={Data[7:0],Data[39:32]};
-        end
+    else if(tx_done)begin
+        Data1<={Data1[7:0],Data1[39:8]};
+    end
     else//IDLE
         Data1<=Data1;
 end
 /*----------------------different data--------------------------*/
 always @(posedge sys_clk or negedge rst_n) begin
     if (!rst_n) begin
-        data <= 40'd0;
+        data <= 7'd0;
     end
-    else if(state)begin //send state
-        if (Data_cnt==0) begin
-            data<=Data[7:0];
-        end
-        else if(Data_cnt) begin //Data_cnt!=0 eg:1,2,3,4 
-            data<=Data1[7:0];
-        end
-        else
-            data<=data;
+    else if(send_go)begin //send state
+        data<=Data1[7:0];
     end
-    else if(state==0)begin //IDLE state
-        data<=40'd0;
-    end
+    else
+        data<=data;
 end
 /*----------------------send_go--------------------------*/
 always @(posedge sys_clk or negedge rst_n) begin
     if (!rst_n) begin
-        send_go<=0;
+        send_go<=1;
     end
     else if(state) begin//send state
-        if(tx_done)begin
+        if((Data_cnt<max)&&(tx_done))begin
             send_go<=1;
         end
         else
             send_go<=0;
     end
     else//IDLE
-        send_go<=0;
+        send_go<=0;//gaile
 end
 /*----------------------all_done--------------------------*/
 always @(posedge sys_clk or negedge rst_n) begin
     if (!rst_n) begin
         all_done<=0;
     end
-    else if((Data_cnt==3'd4)&&(tx_done))begin
+    else if((Data_cnt==max)&&(tx_done))begin
         all_done<=1;
     end
     else
-        all_done<=0;
+        all_done<=all_done;
 end
 endmodule
