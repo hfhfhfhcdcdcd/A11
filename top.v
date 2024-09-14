952bd37c93fee0989f57a3b9b4c5636b4713af02
hfhfff <dyftfboys@163.com>
2024-09-14 11:02:08 +0800

    40 bit ,加入了all_done,trans_go,data_cnt,data1这些变量。 使用了“循环左移8位”的写法。现在我在思考， 变量“state”存在的意义是什么？

--- top.v
+++ top.v
@@ -3,29 +3,34 @@ module top (
     input rst_n,
     output uart_tx
 );
-
+/*----------------------declaration--------------------------*/
+reg Trans_go;
+reg [39:0]Data;
+wire all_done;
 state state1(
 .sys_clk  (sys_clk ),
 .rst_n    (rst_n   ),
 .Trans_go (Trans_go),
 .Data     (Data    ),
-.uart_tx  (uart_tx )
+.uart_tx  (uart_tx ),
+.all_done (all_done)
 );
-/*----------------------declaration--------------------------*/
-reg Trans_go;
-reg [39:0]Data;
+
 /*----------------------Trans_go--------------------------*/
 always @(posedge sys_clk or negedge rst_n) begin
     if (!rst_n) begin
-        Trans_go <= 1;
+        Trans_go <= 0;
+    end
+    else if (all_done) begin//i have led the "all_done signal" to the top module 
+        Trans_go <= 0;
     end
     else 
-        Trans_go <= 0;
+        Trans_go <= 1;
 end
 /*----------------------Data--------------------------*/
 always @(posedge sys_clk or negedge rst_n) begin
     if (!rst_n) begin
-        Data <= 40'h1008040201;//IDLE
+        Data <= 40'h10_08_04_02_01;//IDLE
     end
     else begin
         Data <= Data;
